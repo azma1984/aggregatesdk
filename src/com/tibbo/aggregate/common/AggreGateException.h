2522cec5b56410bdf2d7690792636706f311069f
AzanovAA <azanov@bk.ru>
2015-01-07 16:58:28 +0600

    no message

--- src/com/tibbo/aggregate/common/AggreGateException.h
+++ src/com/tibbo/aggregate/common/AggreGateException.h
@@ -1,22 +1,13 @@
-// Generated from /aggregate_sdk_5.11.00/src/com/tibbo/aggregate/common/AggreGateException.java
-
 #pragma once
+#include <string>
 
-#include <com/tibbo/aggregate/common/fwd-aggregate_sdk_5.11.00.h"
-//#include <java/lang/fwd-aggregate_sdk_5.11.00.h"
-//#include <java/lang/Exception.h"
-
-
-
-class com::tibbo::aggregate::common::AggreGateException
-    : public ::java::lang::Exception
+class AggreGateException
 {
 
-public:
-    typedef ::java::lang::Exception super;
-
 private:
-    std::string* details;
+    std::string details;
+    std::string message;
+
 protected:
     void ctor(std::string* message);
     void ctor(std::string* message, std::string* details);
@@ -25,21 +16,17 @@ protected:
     void ctor(std::string* message, ::java::lang::Throwable* cause, std::string* details);
 
 public:
-    std::string* getDetails();
+    std::string getDetails() { return details; }
 
-    // Generated
-    AggreGateException(std::string* message);
-    AggreGateException(std::string* message, std::string* details);
-    AggreGateException(::java::lang::Throwable* cause);
-    AggreGateException(std::string* message, ::java::lang::Throwable* cause);
-    AggreGateException(std::string* message, ::java::lang::Throwable* cause, std::string* details);
-protected:
-    AggreGateException(const ::default_init_tag&);
+    AggreGateException(const std::string &message)
+    {
+        this->message = message;
+    }
 
+    AggreGateException(const std::string &message, const std::string &details)
+    {
+        this->message = message;
+        this->details = details;
+    }
 
-public:
-    
-
-private:
-    ::java::lang::Class* getClass0();
 };
