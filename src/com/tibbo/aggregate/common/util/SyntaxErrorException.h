51e049e12f50aac93c64bbe3396ea6fcba5307bf
Maxim Bratuh <unimaxis@mail.ru>
2015-01-07 13:16:19 +0200

    SString.cpp

--- src/com/tibbo/aggregate/common/util/SyntaxErrorException.h
+++ src/com/tibbo/aggregate/common/util/SyntaxErrorException.h
@@ -6,6 +6,7 @@
 class SyntaxErrorException : public AggreGateException
 {
 public:
+	//TODO: Throwable
     SyntaxErrorException(const std::string& message) : AggreGateException(message) {}
     SyntaxErrorException(Throwable* cause) : AggreGateException(cause) {}
     SyntaxErrorException(const std::string& message, Throwable* cause) : AggreGateException(message, cause) {}	
--- src/com/tibbo/aggregate/common/util/Util.h
+++ src/com/tibbo/aggregate/common/util/Util.h
@@ -1,42 +1,33 @@
-// Generated from /aggregate_sdk_5.11.00/src/com/tibbo/aggregate/common/util/Util.java
-
-#pragma once
- /*
-
+#ifndef _Util_H_
+#define _Util_H_
+ 
 class Util
-    
 {
 
-public:
-    typedef void super;
-    static bool equals(void* o1, void* o2);
-    static ::java::lang::Throwable* getCause(::java::lang::Throwable* th, ::java::lang::Class* throwableClass);
-    static ::java::lang::Throwable* getRootCause(::java::lang::Throwable* th);
-    static ::int8_tArray* readStream(std::iostream* is) ;
-    static ::java::lang::Number* convertToNumber(void* value, bool validate, bool allowNull);
-    static ::java::lang::Boolean* convertToBoolean(void* value, bool validate, bool allowNull);
+public:    
+	template <class left, class right> static bool equals(left& l, right& r);
+	//TODO:
+	/*
+    static Throwable getCause(Throwable& th, Class<InterruptedException> throwableClass);
+    static Throwable getRootCause(Throwable& th);
+	*/
+    static std::vector<char> readStream(std::iostream& is) ;
+    static Number convertToNumber(void* value, bool validate, bool allowNull);
+    static bool convertToBoolean(void* value, bool validate, bool allowNull);
     static bool isFloatingPoint(::java::lang::Number* n);
-    static const std::string getObjectDescription(void* o);
-    static ::java::lang::Class* getListElementType(::java::lang::reflect::Type* listType);
-    static ::java::lang::Class* getMapKeyType(::java::lang::reflect::Type* mapType);
-    static int parseVersion(std::string* version);
-    static const std::string nameToDescription(std::string* name);
-    static const std::string descriptionToName(std::string* value);
-    static const std::string getTrayIconId(std::string* prefix);
-    static std::list  getIconImages(std::string* prefix, std::list  sizes);
+    static std::string getObjectDescription(void* obj) const;
+	//TODO:
+	/* 
+    static Class* getListElementType(Type* listType);
+    static Class* getMapKeyType(Type* mapType);
+	*/
+    static int parseVersion(const std::string& version);
+    static std::string nameToDescription(const std::string& name);
+    static std::string descriptionToName(const std::string& value);
+    static std::string getTrayIconId(const std::string& prefix);
+	//TODO: Image
+    static std::list<Image> getIconImages(const std::string& prefix, std::list<int>&  sizes);
 	static std::map sortByValue(std::map map);
-
-    // Generated
-    Util();
-protected:
-    Util(const ::default_init_tag&);
-
-
-public:
-    
-    bool equals(void* arg0);
-
-private:
-    ::java::lang::Class* getClass0();
-    friend class Util_sortByValue_1;
-}; */
+	template <K, V> static Map<K, V> sortByValue(Map<K, V>& map) 
+}; 
+#endif	//_Util_H_
