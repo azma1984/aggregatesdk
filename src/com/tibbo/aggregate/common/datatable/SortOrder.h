dd5da3d6801be4bcbf44e35f7628964676c8806d
AzanovAA <azanov@bk.ru>
2015-01-07 17:04:14 +0600

    no message

--- src/com/tibbo/aggregate/common/datatable/SortOrder.h
+++ src/com/tibbo/aggregate/common/datatable/SortOrder.h
@@ -1,42 +1,37 @@
-// Generated from /aggregate_sdk_5.11.00/src/com/tibbo/aggregate/common/datatable/SortOrder.java
-
 #pragma once
+#include <string>
 
-//#include <fwd-aggregate_sdk_5.11.00.h"
-#include <com/tibbo/aggregate/common/datatable/fwd-aggregate_sdk_5.11.00.h"
-//#include <java/lang/fwd-aggregate_sdk_5.11.00.h"
-//#include <java/lang/Object.h"
-
-
-
-class com::tibbo::aggregate::common::datatable::SortOrder
-    
+class SortOrder
 {
-
-public:
-    typedef void super;
-
 private:
-    std::string* field;
-    bool ascending;
-protected:
-    void ctor(std::string* field, bool ascending);
+    std::string     field;
+    bool            ascending;
 
 public:
-    std::string* getField();
-    void setField(std::string* field);
-    bool isAscending();
-    void setAscending(bool ascending);
+  SortOrder(const std::string &field, bool ascending)
+  {
+    this.field = field;
+    this.ascending = ascending;
+  }
 
-    // Generated
-    SortOrder(std::string* field, bool ascending);
-protected:
-    SortOrder(const ::default_init_tag&);
+  std::string getField()
+  {
+    return field;
+  }
 
+  void setField(const std::string &field)
+  {
+    this.field = field;
+  }
 
-public:
-    
+  bool isAscending()
+  {
+    return ascending;
+  }
 
-private:
-    ::java::lang::Class* getClass0();
+  void setAscending(bool ascending)
+  {
+    this.ascending = ascending;
+  }
+
 };
