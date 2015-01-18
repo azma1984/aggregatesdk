5e0e8befda282e33881ccef299fe6ff7688b46da
Maxim Bratuh <unimaxis@mail.ru>
2015-01-18 22:40:40 +0200

    no mess

--- src/com/tibbo/aggregate/common/context/DefaultContextEventListener.h
+++ src/com/tibbo/aggregate/common/context/DefaultContextEventListener.h
@@ -1,31 +1,20 @@
-// Generated from /aggregate_sdk_5.11.00/src/com/tibbo/aggregate/common/context/DefaultContextEventListener.java
-
 #pragma once
 
-//#include <fwd-aggregate_sdk_5.11.00.h"
-//#include <com/tibbo/aggregate/common/context/fwd-aggregate_sdk_5.11.00.h"
-//#include <com/tibbo/aggregate/common/data/fwd-aggregate_sdk_5.11.00.h"
-//#include <com/tibbo/aggregate/common/expression/fwd-aggregate_sdk_5.11.00.h"
-//#include <java/lang/fwd-aggregate_sdk_5.11.00.h"
-//#include <java/lang/Object.h"
-#include <com/tibbo/aggregate/common/event/ContextEventListener.h"
+#include "event/ContextEventListener.h"
+#include "context/CallerController.h"
+#include "expression/Expression.h"
+#include "expression/Evaluator.h"
 
 
-
-class com::tibbo::aggregate::common::context::DefaultContextEventListener
-    
-    , public ContextEventListener
+class DefaultContextEventListener : public ContextEventListener
 {
-
-public:
-    typedef void super;
-
 private:
     CallerController* callerController;
     int  listenerCode;
-    ::com::tibbo::aggregate::common::expression::Expression* filter;
+    Expression* filter;
     bool acceptEventsWithoutListenerCode;
-    ::com::tibbo::aggregate::common::expression::Evaluator* evaluator;
+    Evaluator* evaluator;
+
 protected:
     void ctor();
     void ctor(CallerController* callerController);
@@ -39,12 +28,12 @@ public:
     int  getListenerCode();
     void setCallerController(CallerController* callerController);
 
-public: /* protected */
+protected:
     void setAcceptEventsWithoutListenerCode(bool acceptEventsWithoutListenerCode);
 
 public:
-    void setFilter(::com::tibbo::aggregate::common::expression::Expression* filter);
-    ::com::tibbo::aggregate::common::expression::Expression* getFilter();
+    void setFilter(Expression* filter);
+    Expression* getFilter();
 
     // Generated
     DefaultContextEventListener();
@@ -52,13 +41,4 @@ public:
     DefaultContextEventListener(CallerController* callerController, int  listenerCode);
     DefaultContextEventListener(CallerController* callerController, int  listenerCode, ::com::tibbo::aggregate::common::expression::Expression* filter);
     DefaultContextEventListener(int  listenerCode);
-protected:
-    DefaultContextEventListener(const ::default_init_tag&);
-
-
-public:
-    
-
-private:
-    ::java::lang::Class* getClass0();
 };
