3f6ad2a1a4365e384e9ce7d51c4f290b2b7613c2
AzanovAA <azanov@bk.ru>
2015-01-30 19:16:52 +0600

    no message

--- src/com/tibbo/aggregate/common/datatable/ValidationException.h
+++ src/com/tibbo/aggregate/common/datatable/ValidationException.h
@@ -1,33 +1,16 @@
-// Generated from /aggregate_sdk_5.11.00/src/com/tibbo/aggregate/common/datatable/ValidationException.java
-
-#ifndef ValidationExceptionH
-#define ValidationExceptionH
-
-#include "DataTableException.h"
-
+#pragma once
+#include "datatable/DataTableException.h"
 
 
 class ValidationException: public DataTableException
 {
-/*
 public:
-    typedef DataTableException super;
-protected:
-    void ctor(const std::string & message);
-    void ctor(::java::lang::Throwable* cause);
-    void ctor(const std::string & message, ::java::lang::Throwable* cause);
+   ValidationException(const std::string & message):DataTableException(message)
+   {
+   }
 
-    // Generated
-
-public:
-    ValidationException(const std::string & message);
-    ValidationException(::java::lang::Throwable* cause);
-    ValidationException(const std::string & message, ::java::lang::Throwable* cause);
-protected:
-    ValidationException(const ::default_init_tag&);
-
-
-*/
+   ValidationException(const std::string &message, const std::string &details):DataTableException(message, details)
+   {
+   }
 };
 
-#endif
