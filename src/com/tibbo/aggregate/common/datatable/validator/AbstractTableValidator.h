5867b99f208548d18da11297966b759161516890
AzanovAA <azanov@bk.ru>
2015-02-01 20:24:25 +0600

    no message

--- src/com/tibbo/aggregate/common/datatable/validator/AbstractTableValidator.h
+++ src/com/tibbo/aggregate/common/datatable/validator/AbstractTableValidator.h
@@ -1,21 +1,7 @@
-#ifndef _AbstractTableValidator_H_
-#define _AbstractTableValidator_H_
+#pragma once
 
 #include "datatable/validator/TableValidator.h"
 
-
 class AbstractTableValidator : public TableValidator
 {
-/*
-public:
-    typedef void super;
-    bool equals(void* obj);
-    void* clone();
-
-    // Generated
-    AbstractTableValidator();
-protected:
-    AbstractTableValidator(const ::default_init_tag&);
-*/
 };
-#endif
