e3758b096433c52a9f0ed4d6fb330d41653a8679
AzanovAA <azanov@bk.ru>
2015-02-01 20:12:30 +0600

    no message

--- src/com/tibbo/aggregate/common/datatable/validator/AbstractRecordValidator.h
+++ src/com/tibbo/aggregate/common/datatable/validator/AbstractRecordValidator.h
@@ -1,38 +1,7 @@
-#ifndef _AbstractRecordValidator_H_
-#define _AbstractRecordValidator_H_
-
+#pragma once
 #include "datatable/validator/RecordValidator.h"
-#include "util/Util.h"
-
 
 class AbstractRecordValidator : public RecordValidator
 {
 public:
-    //TODO: пересмотреть сравнение
-    bool equals(AbstractRecordValidator* obj) {
-        if (obj == null) {
-          return false;
-        }
-        /*
-        if (! dynamic_cast<RecordValidator*>(obj)) {
-            return false;
-        }
-        */
-
-        return Util::equals(getType(), ((RecordValidator) obj).getType());
-    }
-
-    //TODO: исключение
-    AbstractRecordValidator* clone() {
-        return RecordValidator::clone();
-//        try
-//        {
-//          return RecordValidator::clone();
-//        }
-//        catch (CloneNotSupportedException ex)
-//        {
-//          throw new IllegalStateException(ex);
-//        }
-    }
 };
-#endif
